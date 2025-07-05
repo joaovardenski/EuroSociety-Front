8a3be6f38f0ea058692bdefc1c0c3822b4dc1593
João Victor Vardenski de Andrade <126905465+joaovardenski@users.noreply.github.com>
2025-07-04 22:32:16 -0300

    Adicionar paleta de cores e fonte principal

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,6 +1,6 @@
 function Login() {
     return (
-        <div className='w-full h-screen flex items-center justify-center bg-gray-100'>
+        <div className='w-full h-screen flex items-center justify-center bg-azulBase'>
             <h1>Login Page</h1>
             <p>This is the login page. Please enter your credentials.</p>
             {/* Add your login form here */}
